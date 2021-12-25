5ed427a382a8a833436e2dadf9fa6383fb9e2441
FM190 <86798311+FM190@users.noreply.github.com>
2021-12-25 13:53:59 -0500

    Add files via upload

---
+++ screens/DashboardScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { NavigationContainer} from '@react-navigation/native';
+import {View, Text} from "react-native";
+
+export default class  HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+        Dashboard
+        </Text>
+      </View>
+    );
+  }
+}
+
